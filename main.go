9bb4a56f4cd84ba9721f1744ab3c818a0840aef3
ynafey <ynafey@gmail.com>
2025-01-12 22:30:50 -0500

    Daily hourly minute

--- main.go
+++ main.go
@@ -8,53 +8,113 @@ import (
 	"io"
 	"net/http"
 	"encoding/json"
+	"time"
 	"os"
 	_ "github.com/mattn/go-sqlite3"
 )
 
-func openDb() {
-	db, err := sql.Open("sqlite3", "./events.db")
+var db *sql.DB
+
+type Message struct {
+	Event string
+}
+
+type EventRow struct {
+	Id int64
+	Timestamp int64
+	Event string
+	Count int64
+}
+
+
+func dailyEvent(sodTimestamp int64, msgEvent string) {
+	row := db.QueryRow("select * from days where timestamp = ? and event = ?", sodTimestamp, msgEvent)
+
+	var eventRow EventRow
+
+	err := row.Scan(&eventRow.Id, &eventRow.Timestamp, &eventRow.Event, &eventRow.Count)
 	if err != nil {
-		log.Fatal(err)
-	}
-	defer db.Close()
-
-
-	rows, err := db.Query(`SELECT id, name FROM example`)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer rows.Close()
-
-	for rows.Next() {
-		var id int
-		var name string
-		rows.Scan(&id, &name)
-		fmt.Printf("User %d: %s\n", id, name)
+		// create a row
+		db.Exec("insert into days (timestamp, event, count) values (?, ?, ?)", sodTimestamp, msgEvent, 1)
+	} else {
+		// increment a row
+		rowId := eventRow.Id
+		nextCount := eventRow.Count + 1
+		db.Exec("update days set count = ? where id = ?", nextCount, rowId)
 	}
 }
 
-type Message struct {
-    Event string
+
+func hourEvent(sohTimestamp int64, msgEvent string) {
+	row := db.QueryRow("select * from hours where timestamp = ? and event = ?", sohTimestamp, msgEvent)
+
+	var eventRow EventRow
+
+	err := row.Scan(&eventRow.Id, &eventRow.Timestamp, &eventRow.Event, &eventRow.Count)
+	if err != nil {
+		// create a row
+		db.Exec("insert into hours (timestamp, event, count) values (?, ?, ?)", sohTimestamp, msgEvent, 1)
+	} else {
+		// increment a row
+		rowId := eventRow.Id
+		nextCount := eventRow.Count + 1
+		db.Exec("update hours set count = ? where id = ?", nextCount, rowId)
+	}
+}
+
+
+func minuteEvent(somTimestamp int64, msgEvent string) {
+	row := db.QueryRow("select * from minutes where timestamp = ? and event = ?", somTimestamp, msgEvent)
+
+	var eventRow EventRow
+
+	err := row.Scan(&eventRow.Id, &eventRow.Timestamp, &eventRow.Event, &eventRow.Count)
+	if err != nil {
+		// create a row
+		db.Exec("insert into minutes (timestamp, event, count) values (?, ?, ?)", somTimestamp, msgEvent, 1)
+	} else {
+		// increment a row
+		rowId := eventRow.Id
+		nextCount := eventRow.Count + 1
+		db.Exec("update minutes set count = ? where id = ?", nextCount, rowId)
+	}
 }
 
 func handleEvent(w http.ResponseWriter, r *http.Request) {
-	log.Println("Receieved Event")
-
 	decoder := json.NewDecoder(r.Body)
 
-    var t Message
-    err := decoder.Decode(&t)
-    if err != nil {
-        panic(err)
-    }
+	var t Message
+	err := decoder.Decode(&t)
+	if err != nil {
+		panic(err)
+	}
+	msgEvent := t.Event
+
+	log.Println("Event Receieved", msgEvent)
+
+	currentTime := time.Now()
+
+	startOfMinute := currentTime.Truncate(time.Minute)
+	somTimestamp := startOfMinute.Unix()
+
+	startOfHour := currentTime.Truncate(time.Hour)
+	sohTimestamp := startOfHour.Unix()
+
+	startOfDay := time.Date(currentTime.Year(), currentTime.Month(), currentTime.Day(), 0, 0, 0, 0, currentTime.Location(), )
+	sodTimestamp := startOfDay.Unix()
+
+	minuteEvent(somTimestamp, msgEvent)
+	hourEvent(sohTimestamp, msgEvent)
+	dailyEvent(sodTimestamp, msgEvent)
 
 	io.WriteString(w, "OK")
 }
 
 func main() {	
+	db, _ = sql.Open("sqlite3", "./events.db")
+
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", handleEvent)
+	mux.HandleFunc("/event", handleEvent)
 
 	fmt.Println("Starting server on port 3333")
 
